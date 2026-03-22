0b2888a42aae196f9ed5f5a1bcc689b614d278c5
Dima-IT <251813@astanait.edu.kz>
2026-03-22 20:06:54 +0500

    1as

---
+++ src/Task2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static int sum(int[] arr, int n) {
+        if (n == 0) return 0;
+        return arr[n - 1] + sum(arr, n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // размер массива
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); // ввод элементов
+        }
+
+        double avg = (double) sum(arr, n) / n;
+        System.out.println(avg);
+    }
+}
